9ce253c931178f49090c7e4dd81322cf4b164852
Wouter <wouterkitsz@gmail.com>
2018-08-02 11:54:59 +0200

    bracketID aanpassingen en opschoning
    ID komt van Tournament
    ID is string vanwege hexadecimaal karakter

--- Tournament/src/Tournament/Bracket.java
+++ Tournament/src/Tournament/Bracket.java
@@ -1,39 +1,31 @@
 package Tournament;
 
 public class Bracket {
+	private String bracketName;
 	private int numOfTeams;
-	ArrayList<Bracket> bracket;
-	private int bracketID;
+	private String bracketID;
 	private int roundNum;
 	// comment
 	
-	Bracket(int bracketID, int numOfTeams, int roundNum) {
-		public void setNumOfTeams(int numOfTeams) {
-			this.numOfTeams = numOfTeams;
+	Bracket(String bracketID, int numOfTeams, int roundNum) {
+		this.numOfTeams = numOfTeams;
+		this.bracketID = bracketID;
+		this.roundNum = roundNum;
 		}
-		public void setBracketID(int bracketID) {
-			this.bracketID = bracketID;
-		}
-		public void setRoundNum(int roundNum) {
-			this.roundNum = roundNum;
-		}
-	}
 	
 	public boolean buildBracket(int numOfTeams) {
 		ArrayList String bracket = new Array[numOfTeams[]; /*even uitzoeken hoe arrays */
-	
-		
-	}
+		}
 	public int getNumOfTeams() {
 		return numOfTeams;
 	}
 	public void setNumOfTeams(int numOfTeams) {
 		this.numOfTeams = numOfTeams;
 	}
-	public int getBracketID() {
+	public String getBracketID() {
 		return bracketID;
 	}
-	public void setBracketID(int bracketID) {
+	public void setBracketID(String bracketID) {
 		this.bracketID = bracketID;
 	}
 	public int getRoundNum() {
