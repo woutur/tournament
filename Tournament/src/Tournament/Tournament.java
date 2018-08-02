09756a3e2a0f522396dfa60c2fd56cd7a815839a
Wouter <wouterkitsz@gmail.com>
2018-08-02 12:03:52 +0200

    bracket en ID toegevoegd

--- Tournament/src/Tournament/Tournament.java
+++ Tournament/src/Tournament/Tournament.java
@@ -8,13 +8,20 @@ public class Tournament {
 	private int numOfParticipants;
 	private String name = "TestTournament" ;  
 	private int sportsType = 1;  // definieren we later wel als basketbal
+	private String bracketID;
+	private String tournamentID;
 
 	public Tournament(int type, int rounds, int numOfTeams, String name) {
 			this.tournamentType = type;
 			this.numOfRounds = rounds;
 			this.numOfTeams = numOfTeams;
 			this.name = name;
+			GenerateUUID tournamentID = new GenerateUUID();
+			this.tournamentID = tournamentID.generateID();
 		
 		}
-		
+	public String createBracket(){ 
+		Bracket bracket = new Bracket(numOfRounds, numOfTeams
+		GenerateUUID bracket = new GenerateUUID();
+		this.bracketID = bracket.generateID();
 	}
